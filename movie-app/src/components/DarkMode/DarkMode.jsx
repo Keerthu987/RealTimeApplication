69c9ac50a4a3ce7079598883ebf97c565d805db9
Keerthu987 <keerthananilasha111@gmail.com>
2025-07-07 19:17:57 +0530

    movie-appdone

--- movie-app/src/components/DarkMode/DarkMode.jsx
+++ movie-app/src/components/DarkMode/DarkMode.jsx
@@ -1,23 +1,47 @@
 import React from "react";
-
 import "./DarkMode.css";
-import { ReactComponent as Sun } from "./Sun.svg";
-import { ReactComponent as Moon } from "./Moon.svg";
+
+import sunIcon from "./Sun.svg";
+import moonIcon from "./Moon.svg";
 
 const DarkMode = () => {
-    return (
-        <div className='dark_mode'>
-            <input
-                className='dark_mode_input'
-                type='checkbox'
-                id='darkmode-toggle'
-            />
-            <label className='dark_mode_label' htmlFor='darkmode-toggle'>
-                <Sun />
-                <Moon />
-            </label>
-        </div>
-    );
+  const setDarkTheme = () => {
+    document.querySelector("body").setAttribute("data-theme", "dark");
+    localStorage.setItem("selectedTheme", "dark");
+  };
+  const setLightTheme = () => {
+    document.querySelector("body").setAttribute("data-theme", "light");
+    localStorage.setItem("selectedTheme", "light");
+  };
+  const selectedTheme = localStorage.getItem("selectedTheme");
+  if (selectedTheme == "light") {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+
+  const toggleTheme = (e) => {
+    if (e.target.checked) {
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
+  };
+  return (
+    <div className="dark_mode">
+      <input
+        className="dark_mode_input"
+        type="checkbox"
+        id="darkmode-toggle"
+        onChange={toggleTheme}
+        defaultChecked={selectedTheme !== "light"}
+      />
+      <label className="dark_mode_label" htmlFor="darkmode-toggle">
+        <img src={sunIcon} alt="Sun" className="icon sun-icon" />
+        <img src={moonIcon} alt="Moon" className="icon moon-icon" />
+      </label>
+    </div>
+  );
 };
 
 export default DarkMode;
